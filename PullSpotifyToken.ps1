44609795690cf55798a5883ca78c6bf02d426fa6
AyoJared <Jaredwerts2006@gmail.com>
2025-06-19 23:47:07 -0400

    Add initial project files including README, .gitignore, PowerShell script, and Docker Compose configuration

---
+++ PullSpotifyToken.ps1
@@ -0,0 +1,27 @@
+get-content .env | foreach {
+    $name, $value = $_.split('=')
+    set-content env:\$name $value
+}
+
+
+$clientId = $env:client_id
+$clientSecret = $env:client_secret
+
+$headers = @{
+    "Content-Type" = "application/x-www-form-urlencoded"
+}
+$body = @{
+    grant_type    = "client_credentials"
+    client_id     = $clientId
+    client_secret = $clientSecret
+}
+
+$response = Invoke-RestMethod -Method Post -Uri "https://accounts.spotify.com/api/token" -Headers $headers -Body $body
+
+# Print full token without truncation
+$response.access_token
+
+$key = "`nAPI_KEY"
+$value = $response
+$output = "$key=$value"
+Add-Content -Path ".env" -Value $output
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Spotify-last-songs
+``
